0f871c109159b8fd44225af54a9d92d9bb8bfd71
= <=>
2023-02-07 23:30:30 +0000

    get user stats

--- app/api/urls.py
+++ app/api/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from api.views import UserJoined
+from api.views import UserJoined, ShowUserStats
 
 urlpatterns = [
-    path('user_joined/<int:user_id>', UserJoined.as_view())
+    path('user_joined/<int:user_id>', UserJoined.as_view()),
+    path('user_stats/<int:user_id>', ShowUserStats.as_view()),
+    # path('user_stats/<int:user_id>/<int:kills>', UserJoined.as_view()),
 ]
--- app/api/views.py
+++ app/api/views.py
@@ -2,6 +2,7 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from api.models import GameUser
+from api.models import UserStats
 
 class UserJoined(APIView):
 
@@ -13,3 +14,24 @@ class UserJoined(APIView):
             'user_id': user.user_id,
             'first_joined_game': user.first_joined_game
             })
+        
+class ShowUserStats(APIView):
+
+    def get(self, request, **kwargs):
+        
+        try:
+            user = GameUser.objects.get(user_id=kwargs['user_id'])
+        except GameUser.DoesNotExist:
+            return Response({'detail': 'user id does not exist'}, status=404)
+        
+        return_object = {}
+        user_stats = UserStats.objects.filter(user=user)
+        
+        for stat in user_stats:
+            return_object[str(stat.mode)] = {
+                'wins': stat.wins,
+                'kills': stat.kills,
+                'topfrags': stat.topfrags
+                }
+            
+        return Response(return_object)
