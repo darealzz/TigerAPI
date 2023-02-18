43292440268d4d543f637fac5dd97c7fd6388495
= <=>
2023-02-18 17:07:06 +0000

    Unit Testing

--- app/api/tests.py
+++ app/api/tests.py
@@ -1,10 +1,16 @@
+
+# SIMULATES BOT MAKING REQUESTS TO THE SERVER
+
 from django.test import TestCase
 import requests
 import pprint
 
-def get_stats():
-    response = requests.get('http://127.0.0.1:8000/v1/user_stats/123')
-    # response = requests.patch('http://127.0.0.1:8000/v1/user_stats/123/add_weapon_kills/AR-11/2')
+def get_xp():
+    response = requests.get('http://127.0.0.1:8000/v1/user_xp/573280920')
+    pprint.pprint(response.json())
+    
+def update_xp():
+    response = requests.patch('http://127.0.0.1:8000/v1/user_xp/573280920/add_xp/25')
     pprint.pprint(response.json())
 
-get_stats()
+update_xp()
