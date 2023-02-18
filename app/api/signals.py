7f1548133b7c908777a202dee058666a6139a96d
= <=>
2023-02-18 17:06:50 +0000

    Promote Demote system

--- app/api/signals.py
+++ app/api/signals.py
@@ -1,14 +1,17 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
+from django.core.cache import cache
+
 from api.models import GameUser
 from api.models import UserStats
 from api.models import UserWeaponStats
 
-from constants import POSSIBLE_MODES, POSSIBLE_WEAPONS
+from constants import POSSIBLE_MODES, POSSIBLE_WEAPONS, RANK_XP_BINDS, GROUP_ID
+
 
 @receiver(post_save, sender=GameUser)
-def my_callback(sender, instance, created, **kwargs):
+def callback(sender, instance, created, **kwargs):
     if created:
         for mode in POSSIBLE_MODES:
             UserStats.objects.create(
@@ -19,6 +22,15 @@ def my_callback(sender, instance, created, **kwargs):
         for weapon in POSSIBLE_WEAPONS:
             UserWeaponStats.objects.create(
                 user=instance,
-                weapon=weapon,
+                weapon=weapon
             )
-
+    
+    else:
+        last_key = 0
+        for key, value in RANK_XP_BINDS.items():
+            if value > instance.xp:
+                break
+            last_key = key
+        
+        Client = cache.get('Client')
+        Client.change_rank(GROUP_ID, instance.user_id, last_key)
